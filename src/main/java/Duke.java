2757c0de03fce61b2318014947ad848960be9b15
scoot1234 <171323903+scoot1234@users.noreply.github.com>
2026-08-18 10:43:37 +0800

    Changed chatbot name, greet and exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,19 @@
 public class Duke {
     public static void main(String[] args) {
-        String banner = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
+        String divider = "____________________________________________________________";
+        String banner = " ____  _                   \n"
+                + "| __ )| | ___   ___  _ __  \n"
+                + "|  _ \\| |/ _ \\ / _ \\| '_ \\ \n"
+                + "| |_) | | (_) | (_) | |_) |\n"
+                + "|____/|_|\\___/ \\___/| .__/ \n"
+                + "                    |_|    ";
+
+        System.out.println(divider);
         System.out.println(banner);
+        System.out.println("Hello! I'm Bloop.");
+        System.out.println("What can I do for you?");
+        System.out.println(divider);
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(divider);
     }
 }
